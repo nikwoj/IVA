376df37c89c3215c48e006deebfd8b9bbcdcb27a
Nikolas Wojtalewicz <nikolas.wojtalewic13@ncf.edu>
2015-10-01 04:36:00 -0400

    added unit testing stuff

--- 9_24/version9/dIVA_L.py
+++ 9_24/version9/dIVA_L.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.linalg import pinv, det
-from numpy import dot
+from numpy import dot, log, sqrt
 
 from scipy.optimize import fmin_l_bfgs_b
 
@@ -11,16 +11,16 @@ def dIVA_L(X, W_init=[], verbose=False) :
     N,R = X[0].shape[0], X[0].shape[1]
     K   = [x.shape[2] for x in X]
     P   = len(K)
-    KK  = [sum(K[0:p]) for p in range(P)]
+    KK  = [sum(K[0:p+1]) for p in range(P)]
     KK.append(0)
     
     if W_init == [] :
-        W_init = [np.random.rand(N,N,K[p]-K[p-1]) for p in range(P)]
+        W_init = [np.random.rand(N,N,K[p]) for p in range(P)]
     W_init = mat_to_vec(W_init)
     W,d,i = fmin_l_bfgs_b(cost_and_grad, x0=W_init)
     if verbose :
-        print "Optimization finished"
-    W = vec_to_mat(W,N,KK[p-2])
+        print "Optimization Finished"
+    W = vec_to_mat(W,N,KK[-2])
     W_m = []
     for p in range(P) :
         W_m.append(W[:,:,KK[p-1]:KK[p]])
@@ -31,43 +31,45 @@ def set_para (X, verbose) :
     N,R = X[0].shape[0], X[0].shape[1]
     P   = len(X)
     K   = [x.shape[2] for x in X]
-    KK  = [sum(K[0:p]) for p in range(P)]
+    KK  = [sum(K[0:p+1]) for p in range(P)]
     KK.append(0)
-    W_m    = []
-    disper = []
-    Y = []
-
+    
     def cost_and_grad(W) :
+        print "Running"
         W = vec_to_mat(W,N,KK[-2])
+        W_m = []
+        disper = []
+        Y = [np.zeros((N,R,k)) for k in K]
         for p in range(P) :
             W_m.append(W[:,:,KK[p-1]:KK[p]])
             for k in range(K[p]) :
-                Y.append(dot(W_m[p][:,:,k], X[p][:,:,k]))
-        #rm(W)
+                Y[p][:,:,k] = dot(W_m[p][:,:,k], X[p][:,:,k])
         disper = [np.array([dot(Y[p][n,:,:].T, Y[p][n,:,:]) for n in range(N)]) for p in range(P)]
-        A = [np.array([dot(pinv(disper[p][n,:,:]), Y[p][n,:,:]) for n in range(N)]) for p in range(P)]
+        A = [np.array([dot(pinv(disper[p][n,:,:]), Y[p][n,:,:].T) for n in range(N)]) for p in range(P)]
         YdY = np.zeros((N,R))
         for n in range(N) :
-            YdY[n,:] = sum([dot(Y[p][n,:,:].T, A[p][n,:,:]) for p in range(P)])
+            YdY[n,:] = np.sum(sum([Y[p][n,:,:].T * A[p][n,:,:] for p in range(P)]), 0)
+            #for p in range(P) :
+                #YdY[n,:] = YdY[n,:] + np.sum(Y[p][n,:,:].T * A[p][n,:,:], 0)
         YdY = np.sqrt(YdY)
         cost = np.sum(YdY) * (np.sqrt(R-1)/R)
-        if verbose :
-            print cost
         for p in range(P) :
             for k in range(K[p]) :
                 cost = cost - log(abs(det(W_m[p][:,:,k])))
             for n in range(N) :
                 cost = cost + log(det(disper[p][n,:,:])) * (1.0/2.0)
+        if verbose :
+            print cost
         gradient = [np.zeros((N,N,K[p])) for p in range(P)]
         for p in range(P) :
             for n in range(N) :
-                B = A[p][n,:,:] * (((sqrt(R-1)/R)) / YdY )
+                B = A[p][n,:,:] * (((sqrt(R-1)/R)) / YdY[n,:] )
                 C = np.identity(K[p]) - dot(B, Y[p][n,:,:])
                 val = B + dot(C, A[p][n,:,:])
                 for k in range(K[p]) :
-                    gradient[p][n,:,k] = np.dot(val[k,:], X[p][:,:,k])
+                    gradient[p][n,:,k] = np.dot(val[k,:], X[p][:,:,k].T)
             for k in range(K[p]) :
-                gradient[p][:,:,k] = gradient[p][:,:,k] - pinv(W_m[p][:,:,k].T)
-        gradient = mat_to_vec(gradient)
+                gradient[p][:,:,k] = (gradient[p][:,:,k] - pinv(W_m[p][:,:,k]).T)
+        gradient = np.array(mat_to_vec( gradient ))
         return cost, gradient
     return cost_and_grad
--- 9_24/version9/test_dIVA_L.py
+++ 9_24/version9/test_dIVA_L.py
@@ -6,19 +6,19 @@ from joint_isi import joint_ISI
 from scipy.io import loadmat
 
 def main() :
-    S = np.zeros((16,32968,4))
+    S = np.zeros((16,3000,4))
     X = S.copy()
     Xm = []
     A = np.random.rand(16,16,4)
     for i in range(4) :
-        S[:,:,i] = loadmat("SCV_IVA_case12_r001.mat")['Sgt'][0,i][:,:]
+        S[:,:,i] = loadmat("SCV_IVA_case12_r001.mat")['Sgt'][0,i][:,0:3000]
         X[:,:,i] = np.dot(A[:,:,i], S[:,:,i])
     Xm.append(X[:,:,0:2])
     Xm.append(X[:,:,2:4])
     joint = 1
+    A_m = [A[:,:,0:2], A[:,:,2:4]]
     while joint>0.1 :
-        W,_,_ = dIVA_L(Xm)
-        joint = joint_ISI(W, A)
-        print joint
+        W,_,_ = dIVA_L(Xm, verbose=True)
+        joint = joint_ISI(W, A_m)
     
 main()        
